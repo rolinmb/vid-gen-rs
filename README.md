f35188eb46a6eab281a3bbbe869784e2837bb6bb
Rolin Blake <rolinmblake@gmail.com>
2024-01-16 06:45:21 -0600

    removing code that I wont be pursuing

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 Generating .mp4 videos using rust image and ffmpeg, similar to my vid-gen-go and vid-gen repos
 
 main.rs:
-    - Main generation driver to build .png image frames and call ffmpeg to glue them into a .mp4 file
+- Main generation driver to build .png image frames and call ffmpeg to glue them into a .mp4 file
     Example: (from project root) go build -C src -o main.exe && cargo build && cargo run
 
 main.go:
-    - Used to create executables that can be used for .png image pixel color calculations by x,y coordinates
-    - To use the golang output binary, replace "(" w/ [" and ")" w/ "]" and also wrap expression with an extra set of "[]" as such (3 total arguments):
-        -> (from src) go build -o main.exe && ./main.exe [[[pow[y,2+x]]/[1+x]]*sin[y]] 2 10 
-        -> (output) -1813.40370
+- Used to create executables that can be used for .png image pixel color calculations by x,y coordinates
+- To use the golang output binary, replace "(" w/ [" and ")" w/ "]" and also wrap expression with an extra set of "[]" as such (3 total arguments):
+    -> (from src) go build -o main.exe && ./main.exe [[[pow[y,2+x]]/[1+x]]*sin[y]] 2 10 
+    -> (output) -1813.40370
 
 parser.go:
-    - evaluateASTNode uses go standard libraries to take the result of parsing a string with go/parser (interface{} type object) and two variables x,y ; then outputs the result
+- evaluateASTNode uses go standard libraries to take the result of parsing a string with go/parser (interface{} type object) and two variables x,y ; then outputs the result
 
-TODO: finish implementing custom string defined function parsing:
-    - currently has to parse the expression string for every pixel for r,g,b respectively; want to build then call executables for r,g,b functions and parse the output into u32 / f64 in rust for each pixel
+TODO:
+- decompose .mp4 video into frames and then process with FX and rebuild like in vid-gen-go
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 use std::fs;
 use std::path::Path;
 use std::process::Command;
-use image::{Rgba, RgbaImage};/*
+use image::{Rgba, RgbaImage};
 
-fn fneval(stringexpr: &str, x: u32, y: u32) -> f64 {
-  //println!("{}", stringexpr);
-  let mut cmdexprstr = stringexpr.replace(char::is_whitespace, "");
-  cmdexprstr = cmdexprstr.replace("(", "[");
-  cmdexprstr = cmdexprstr.replace(")", "]");
-  cmdexprstr = "[".to_owned()+&cmdexprstr;
-  cmdexprstr = cmdexprstr+"]";
-  //println!("{}", cmdexprstr);
-  let cmdeval = if cfg!(target_os = "windows") {
-    Command::new("cmd")
-      .args(["/C", "src\\main.exe", &cmdexprstr, &x.to_string(), &y.to_string()])
-      .output()
-      .expect("fneval(): Failed to execute eval command")
-  } else {
-    Command::new("sh")
-      .arg("-c")
-      .arg("echo hello from fneval() in main.rs")
-      .output()
-      .expect("fneval(): Failed to execute eval command")
-  };
-  let evaloutput = String::from_utf8_lossy(&cmdeval.stdout);
-  //println!("{}", evaloutput);
-  match evaloutput.trim().parse::<f64>() {
-    Ok(parsednum) => parsednum,
-    Err(err) => {
-      eprintln!("\nfneval(): Error parsing {} to f64: {}", evaloutput, err);
-      0.0
-    }
-  }
-}
-
-*/fn cleandir(dir: &str) -> std::io::Result<()> {
+fn cleandir(dir: &str) -> std::io::Result<()> {
   fs::remove_dir_all(dir)?;
   fs::create_dir_all(dir)?;
   Ok(())
@@ -119,64 +88,15 @@ fn genclosures(
   println!("\ngenclosures(): Successfully executed ffmpeg command to generate {}", vidname);
   gencleanup(pngdir, pngname, frames);
   println!("\ngenclosures(): Successfully cleaned up extra frames in '{}'", pngdir);
-}/*
-
-fn genstrings(
-  pngdir: &str, pngname: &str, vidname: &str,
-  width: u32, height: u32, frames: u32,
-  str_r: &str, str_g: &str, str_b: &str,
-) {
-  if !fs::metadata(pngdir).is_ok() {
-    if let Err(err) = fs::create_dir_all(pngdir) {
-      eprintln!("genstrings(): Error creating directory '{}': {}", pngdir, err);
-    } else {
-      println!("\ngenstrings(): Successfully created '{}'", pngdir);
-    }
-  } else {
-    println!("genstrings(): '{}' already exists; cleaning directory contents...", pngdir);
-    if let Err(err) = cleandir(pngdir) {
-      eprintln!("genstrings(): Error cleaning '{}': {}", pngdir, err);
-    }
-    println!("\ngenstrings(): Successfully cleaned '{}'", pngdir);
-  }
-  let mut pngframe = RgbaImage::new(width, height);
-  for i in 1..frames+1 {
-    for x in 0..width {
-      for y in 0..height {
-        pngframe.put_pixel(
-          x, y,
-          Rgba([fneval(str_r,x,y) as u8, fneval(str_g,x,y) as u8, fneval(str_b,x,y) as u8, 255]),
-        );
-      }
-    }
-    pngframe.save(format!("{}/{}_{}.png", pngdir, pngname, i - 1 )).unwrap();
-    pngframe.save(format!("{}/{}_{}.png", pngdir, pngname, (((frames*2)-1)-(i-1)) )).unwrap();
-  }
-  println!("\ngenstrings(): Successfully generated .png frames for .mp4");
-  let _cmdffmpeg = if cfg!(target_os = "windows") {
-    Command::new("cmd")
-      .args(["/C", &format!("ffmpeg -y -framerate 30 -i {}/{}_%d.png -c:v libx264 -pix_fmt yuv420p {}.mp4", pngdir, pngname, vidname)])
-      .output()
-      .expect("genstrings(): Failed to execute ffmpeg command")
-  } else {
-    Command::new("sh")
-      .arg("-c")
-      .arg("echo hello")
-      .output()
-      .expect("genstrings(): Failed to execute ffmpeg command")
-  };
-  println!("\ngenstrings(): Successfully executed ffmpeg command to generate {}", vidname);
-  gencleanup(pngdir, pngname, frames);
-  println!("\ngenstrings(): Successfully cleaned up extra frames in '{}'", pngdir);
 }
 
-*/fn main() {
+fn main() {
   genclosures(
-    &String::from("src/png_out/test1.2"), // pngdir
-    &String::from("test1.2"), // pngname
-    &String::from("src/vid_out/test1.2"), // vidname
+    &String::from("src/png_out/test2.0"), // pngdir
+    &String::from("test2.0"), // pngname
+    &String::from("src/vid_out/test2.0"), // vidname
     1000, 1000, // width, height
-    280, // frames
+    30, // frames
     1.42, 1.125, // scale, scalefactor
     |x, y| (x-y) as f64, // f_r()
     |x, y| (x*y) as f64, // f_g()
@@ -185,15 +105,4 @@ fn genstrings(
     |x, y| (((x*y).cos()) * (x*x - y*y)) as f64, // fb_theta()
     |x, y| (((x*y).tan()) * (x-y)) as f64, // fg_theta()
   );
-  //println!("{}", fneval(&String::from("(((pow(y , 2 + x)) / (1 + x))*sin( y ))"), 2, 10));
-  /*genstrings(
-    &String::from("src/png_out/test1"), // pngdir
-    &String::from("test1"), // pngname
-    &String::from("src/vid_out/test1"), // vidname
-    1000, 1000, // width, height
-    10, // frames
-    &String::from("sin(x)+cos(y)+tan(x+y)"), // str_r
-    &String::from("x*y"), // str_g
-    &String::from("x*x+y*y"), // str_b
-  );*/
 }
